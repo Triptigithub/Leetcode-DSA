1cf25aefa2c36ea277f4350f553baabf8ccaa305
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2025-03-22 11:51:50 +0530

    Time: 34 ms (23.88%) | Memory: 45.3 MB (26.11%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,78 +1,31 @@
-//did on my own
-//here what we did is we fill the elemnt in hashmap one by one
-//now we check ki ek toh max element h aur jo baki element h unki frequebcy ka sum jo h vo given k ke sath kesa h 
-///agar chota ya euqal  ht oh ans compute kro 
-
-//nhi to agar bda h toh shrink kro hmare window
-//now while shrink if you simply do rest-- that will fail bcz
-//ex arr  = b b a z r --> here max element is b if freuency 2
-//sumof all is 5 so rest is 5-2 that is rest is 3
-//now if ypu are doing rest-- you are saying that we are reomving element from (a z r)
-
-//but thats not the case here we are removing max element b here 
-//so make sure whenever you are remiving the element you recompute the rest 
-
+//similar to my previous submit just made code more cleaner
 class Solution {
     public int characterReplacement(String s, int k) {
         HashMap<Character, Integer> map = new HashMap<>();
         int i = 0;
         int j = 0;
         int ans = 0;
+        int maxFreq= 0;
 
         while (j < s.length()) {
             char ch = s.charAt(j);  
-            if (map.containsKey(ch)) {
-                int cnt = map.get(ch);
-                cnt++;
-                map.put(ch, cnt);
-            } else {
-                map.put(ch, 1);
-            }
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(ch));
 
-            // Compute max frequency and sum of frequencies
-            int max = 0;
-            int sum = 0;
-            for (int value : map.values()) {
-                if (value > max) max = value;
-                sum += value;
-            }
-            int rest = sum - max;
-
-
-
-
-
-
-
-
-
+            //here we dont need to explixitylu find out suma nd max
+            //in prev code also sum represent the lengtth of window
+            //and max represent maxfreq
+            
+            int rest = (j-i+1) - maxFreq;
 
             if (rest <= k) {
                 ans = Math.max(ans, j - i + 1);
                 j++;
             } else {
-                while (rest > k) {
                     char first = s.charAt(i);
-                    if (map.containsKey(first)) {
-                        int frequency = map.get(first);
-                        if (frequency <= 1) {
-                            map.remove(first);
-                        } else {
-                            map.put(first, frequency - 1);
-                        }
-                    }
-
-                    // Recompute max and sum only after removing elements
-                    max = 0;
-                    sum = 0;
-                    for (int value : map.values()) {
-                        if (value > max) max = value;
-                        sum += value;
-                    }
-                    rest = sum - max;
-                    
-                    i++;  // Move left pointer
-                }
+                     map.put(first, map.get(first) - 1);
+                     i++;  // Move left pointer
+                
                 j++;
             }
             
