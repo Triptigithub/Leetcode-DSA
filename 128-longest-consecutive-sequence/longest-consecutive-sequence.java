81cd9c4656fb8f42646c56ce5b9d0f80d355d236
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2025-06-12 11:56:03 +0530

    Time: 39 ms (17.94%) | Memory: 67.9 MB (27.80%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,27 +1,37 @@
-//
+//using optimal strriver approach
+//using hashset
+//we will firstly fill the set with array val
+//now loop over set values;
+//check agar current value se pichle va;ue exist krti h toh cur element is not starting pt so skip it
+
+
+//agar cur val se piche vali value exist nhi krti mean cur element is our starting pt
+//now what we will do is we take that element as stsrting no and run a while loop until cur +1 is present in set and also make sure to increase cnt and update cur element
 
 class Solution {
-    public int longestConsecutive(int[] nums) {
-
-        int n = nums.length;
-        if(n==1)return 1;
-        if(n==0)return 0;
-        int ans =1;
-        Arrays.sort(nums);
-        int curnum = nums[0];
-        int cnt=1;
-        for(int i=1;i<n;i++){
-
-              if(nums[i] == curnum+1 ){
-                cnt++;
-                curnum = nums[i];
-                ans = Math.max(ans,cnt);
-             }else if(nums[i] != curnum){
-                curnum = nums[i];
-                cnt = 1;
-             }
-
+    public int longestConsecutive(int[] arr) {
+        int n = arr.length;
+        //fill no in set
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(arr[i]);
         }
-        return ans;
+        int longest = 0;
+
+        //now again loop over set
+        for(int it : set){
+          if (!set.contains(it - 1)) {
+                // find consecutive numbers
+                int cnt = 1;
+                int x = it;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = Math.max(longest, cnt);
+            }
+        }
+        return longest;
+    
     }
 }
