57d1bb29fa4f5f69ab95f3aec8eac26f83b411b6
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2025-11-15 12:37:09 +0530

    Time: 29 ms (21.63%) | Memory: 53.1 MB (5.03%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,22 +1,37 @@
-//brute force
+//memo  = 0(n2)
 class Solution {
     public int countSubstrings(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        // initialize dp with -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
         int cnt = 0;
 
-        for(int i=0; i<s.length();i++){
-            for(int j = i; j<s.length();j++ ){
-                if(solver(s,i,j) == true)cnt++;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solver(s, i, j, dp) == 1) cnt++;
             }
         }
         return cnt;
     }
 
-    public boolean solver(String s, int i,int j){
-            if(i>j)return true;
-            if(s.charAt(i) == s.charAt(j)){
-                return solver(s,i+1,j-1);
-            }
-            return false;
-    }
+    public int solver(String s, int i, int j, int[][] dp) {
+        if (i >= j) return dp[i][j] = 1;
 
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = solver(s, i+1, j-1, dp);
+        } else {
+            dp[i][j] = 0;
+        }
+
+        return dp[i][j];
+    }
 }
