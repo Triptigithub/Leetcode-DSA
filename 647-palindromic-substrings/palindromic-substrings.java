386cbfc146f20a27c548ccdd5674b45637a24623
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2025-11-15 12:30:59 +0530

    Time: 853 ms (5.89%) | Memory: 42.9 MB (39.85%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,24 +1,22 @@
 //brute force
 class Solution {
     public int countSubstrings(String s) {
-      int ans =0; 
-       
-       for(int i=0; i<s.length();i++){
-        ans += solver(s,i,i);
-        ans+=solver(s,i,i+1);
-       }
-       return ans;
-    }
+        int cnt = 0;
 
-    public int solver(String s, int l , int r){
-        int cnt =0;
-
-
-        while(l>=0 &&  r<s.length() &&   s.charAt(l) == s.charAt(r)){
-            cnt++;
-            l--;
-            r++;
+        for(int i=0; i<s.length();i++){
+            for(int j = i; j<s.length();j++ ){
+                if(solver(s,i,j) == true)cnt++;
+            }
         }
         return cnt;
     }
+
+    public boolean solver(String s, int i,int j){
+            if(i>j)return true;
+            if(s.charAt(i) == s.charAt(j)){
+                return solver(s,i+1,j-1);
+            }
+            return false;
+    }
+
 }
