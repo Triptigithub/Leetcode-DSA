823ca22ca98e47610493211eb2deeeb6f00bc943
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2025-12-06 16:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (48.60%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,26 +1,35 @@
 //understand via codewithmik and coded myself
-//memo byme
+//now tabulation
+//did myslef
 class Solution {
-    public int solver(int i, String s,int n,int[] dp){
-        if(i>n)return 0;
-         if(i==n)return 1;
 
-        if(s.charAt(i) == '0' )return 0;
-       
-        if(dp[i]!= -1)return dp[i];
-        int first =solver(i+1,s,n,dp);
-        int second =0;
-        if(i <= n-2){
-        if(s.charAt(i) == '1' ||( s.charAt(i) =='2' && s.charAt(i+1) <= '6')){
-            second = solver(i+2,s,n,dp);
-        }}
-        return dp[i]= first+second;
-    }
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-       return solver(0,s,n,dp);
-        
+        int[] dp = new int[n+1];
+        //array fill nhi krna
+
+        //base case shi kro
+        dp[n]=1;
+
+        for(int i=n-1; i>=0;i--){
+             if(s.charAt(i) == '0' ){
+                dp[i] = 0;
+             }else{
+                     int first =dp[i+1];
+                    int second =0;
+                    if(i <= n-2){
+                        if(s.charAt(i) == '1' ||( s.charAt(i) =='2' && s.charAt(i+1) <= '6')){
+                        second = dp[i+2];
+                        }
+                    }
+                    dp[i]= first+second;
+                }
+        }
+
+
+
+        return dp[0];
+
+
     }
 }
