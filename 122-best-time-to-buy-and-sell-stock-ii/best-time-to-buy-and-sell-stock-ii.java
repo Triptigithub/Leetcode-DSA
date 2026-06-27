b9d704b02d8357852dda90d4dce8831539cfb897
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2026-06-27 11:36:47 +0530

    Time: 1 ms (19.72%) | Memory: 46.5 MB (31.15%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -3,41 +3,43 @@ class Solution {
         //buy = 1 (we can buy stock if we want to we are not holding  any stock)
         //buy = 0 (we prev has buyed one stock we need to sell that before buy any more)
 
+        //here we find out that previously when we were using dp arr
+        //we were actually using the current index arr value at [i][0] and [i][1] and the next index value only [i+1][0] and [i+1][1] 
+        //here we just declare that variable only at last switching them to compute next values
+
         int n = prices.length;
         int profit = 0;
-        int[][] dp = new int[n + 1][2];
-        // for(int i= 0; i<n+1 ; i++){
-        //     for(int j=0; j<2; j++){
-        //         dp[i][j] = -1;
-        //     }
-        // }
+        int[] frontarr = new int[2];
+        int[] curarr = new int[2];
 
+        frontarr[0] = 0;
+        frontarr[1] = 0;
 
         //base case
-        dp[n][0] = 0;
-        dp[n][1] = 0;
+        
 
         for (int i = n - 1; i >= 0; i--) {
             for (int buy = 1; buy >= 0; buy--) {
                 profit = 0;
                 if (buy == 1) {
                     //profit = max(you buy + you did not buy)
-                    profit = Math.max(-prices[i] + dp[i + 1][0],
-                            0 + dp[i + 1][1]);
+                    profit = Math.max(-prices[i] + frontarr[0],
+                            0 + frontarr[1]);
                 } else {
                     //profit = max(you sell stock, you didnot sell stk)
-                    profit = Math.max(prices[i] + dp[i + 1][1],
-                            0 + dp[i + 1][0]);
+                    profit = Math.max(prices[i] + frontarr[1],
+                            0 + frontarr[0]);
                 }
-                dp[i][buy] = profit;
+                curarr[buy] = profit;
             }
+
+            //switching
+            frontarr = curarr;
+
         }
 
-        return dp[0][1];
-        //we return dp[0][1] because this is the state where we start solving problem 
-        //it says we are starting at idx 0 and buy state is 1 means we can buy stk if we want
-        //we cannot return dp[0][0] as ans becasue it sasys we are at index 0 and currently you can sell stk but you didnot buy stk how you are selling
-        //we are able to return dp[0][1] -> bcz this is tabulation we filled ans in reverse frrom base case
+        return frontarr[1];
+      
 
     }
 }
