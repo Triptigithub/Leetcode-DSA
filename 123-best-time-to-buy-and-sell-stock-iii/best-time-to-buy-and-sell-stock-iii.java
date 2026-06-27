f3b23cfa9eeb530b5e0cfee69afd2bf71646682d
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2026-06-27 13:00:10 +0530

    Time: 61 ms (31.44%) | Memory: 118 MB (27.69%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,42 +1,51 @@
 class Solution {
-    public int solver(int i , int buy , int cap, int[] arr, int n, int[][][] dp){
+    public int maxProfit(int[] prices) {
         //buy = 1 (we can buy stock if we want to we are not holding  any stock)
         //buy = 0 (we prev has buyed one stock we need to sell that before buy any more)
 
-        // Base case: If we have processed all stocks or have no capital left, return 0 profit
-        if(i == n || cap == 0) return 0;
+        int n = prices.length;
         int profit = 0;
-
-
-        if(dp[i][buy][cap] != -1)return dp[i][buy][cap];
-       
-        if(buy == 1){
-            //profit = max(you buy + you did not buy)
-            profit = Math.max( -arr[i]+solver(i+1,0,cap,arr,n,dp),
-                                     0+solver(i+1,1,cap,arr,n,dp));
-        }else{
-            //profit = max(you sell stock, you didnot sell stk)
-            //here when you sell stock  you complete one transaction
-            profit = Math.max( arr[i]+solver(i+1,1,cap-1,arr,n,dp),
-                                    0+solver(i+1,0,cap,arr,n,dp));
-        }
+        int[][][] dp = new int[n + 1][2][3];
         
 
-        return dp[i][buy][cap] = profit;
-    }
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int i= 0; i<n ; i++){
-            for(int j=0; j<2; j++){
-                for(int k=0; k<3;k++){
-                dp[i][j][k] = -1;
+        //if(i == n || cap == 0) return 0;
+        //so for i == n -> buy and cap can be anything
+        //1 base case
+        for(int buy = 0; buy<2 ;buy++){
+            for(int cap = 0; cap<3;cap++){
+                dp[n][buy][cap] = 0;
             }
         }
+
+        //if cap == 0 -> i and buy can be anything
+        for(int i =0 ; i<n+1; i++){
+            for(int buy = 0; buy<2; buy++){
+                dp[i][buy][0] = 0;
+            }
         }
-        
-        //idx,buy,cap
-        return solver(0,1,2,prices,n,dp); 
-        //checking how much profit we can make if we start from index 0 when buy state is 1 i.e currently we are not holding any stk so we can buy 
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 1; buy >= 0; buy--) {
+                for(int cap=1; cap<=2; cap++){  //cap start from 1 as we handle cap =0 in base case
+                profit = 0;
+                if (buy == 1) {
+                    //profit = max(you buy + you did not buy)
+                    profit = Math.max(-prices[i] + dp[i + 1][0][cap],
+                            0 + dp[i + 1][1][cap]);
+                } else {
+                    //profit = max(you sell stock, you didnot sell stk)
+                    profit = Math.max(prices[i] + dp[i + 1][1][cap-1],
+                            0 + dp[i + 1][0][cap]);
+                }
+                dp[i][buy][cap] = profit;
+            }
+        }}
+
+        return dp[0][1][2];
+        //we return dp[0][1] because this is the state where we start solving problem 
+        //it says we are starting at idx 0 and buy state is 1 means we can buy stk if we want
+        //we cannot return dp[0][0] as ans becasue it sasys we are at index 0 and currently you can sell stk but you didnot buy stk how you are selling
+        //we are able to return dp[0][1] -> bcz this is tabulation we filled ans in reverse frrom base case
+
     }
 }
