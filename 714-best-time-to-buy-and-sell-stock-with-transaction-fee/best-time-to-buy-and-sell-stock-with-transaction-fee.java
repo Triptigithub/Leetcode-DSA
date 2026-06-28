4f8bacaec5eb1cd5c8c0a8efdec073ac873d8587
Tripti Aggarwal | Software Engineer <97337950+Triptigithub@users.noreply.github.com>
2026-06-28 13:50:02 +0530

    Time: 17 ms (51.06%) | Memory: 70.9 MB (37.19%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,26 +1,4 @@
 class Solution {
-    public int solver(int i, int buy, int[] prices, int n,int fee,int[][] dp){
-        if(i==n)return 0; 
-        
-        
-        int profit =0;
-        if(dp[i][buy] !=-1)return dp[i][buy];
-        if(buy == 1){
-            //deciding buy
-            //profit = max(you buy +you not buy)
-            profit = Math.max(-prices[i]+solver(i+1,0,prices,n,fee,dp),
-                               0 + solver(i+1,1,prices,n,fee,dp)); 
-        }else{
-            //deciding sell
-            //profit = max(sell,not sell)
-            //here in sell case --> when you are complete one transaction you need to subtract the fee from profit
-            profit = Math.max(-fee + prices[i]+solver(i+1,1,prices,n,fee,dp),
-                                     0+ solver(i+1,0,prices,n,fee,dp));
-        }
-
-        return dp[i][buy] = profit;
-
-    }
     public int maxProfit(int[] prices, int fee) {
         //so 
         //buy = 1 means we can buy 
@@ -28,11 +6,33 @@ class Solution {
 
         int n = prices.length;
         int[][] dp = new int[n+1][2];
-        for(int i = 0; i<n+1; i++){
-            Arrays.fill(dp[i],-1);
+
+        //  if (i == n)return 0;
+         dp[n][0] = 0;
+         dp[n][1] = 0;
+        
+        for(int i = n-1 ; i>=0 ; i--){
+            for (int buy = 0; buy < 2; buy++){
+                int profit =0;
+                if(buy == 1){
+                   //deciding buy
+                   //profit = max(you buy +you not buy)
+                   profit = Math.max(-prices[i]+dp[i+1][0],
+                                        0 + dp[i+1][1]); 
+                }else{
+                    //deciding sell
+                    //profit = max(sell,not sell)
+                    //here in sell case --> when you are complete one transaction you need to subtract the fee from profit
+                    profit = Math.max(-fee + prices[i]+dp[i+1][1],
+                                     0+dp[i+1][0]);
+                }
+
+              dp[i][buy] = profit;
+            }
         }
+        
         //we have infinite transaction we can buy ans sell as much time as we want
         //i,buy,prices arr ,arr lengtth,fee,dp
-        return solver(0,1,prices,n,fee,dp);
+        return dp[0][1];
     }
 }
